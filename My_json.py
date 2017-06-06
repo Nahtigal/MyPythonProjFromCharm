4ae6623f2fd2462fa721a90b1bfa43aad93d21f7
petryk <petryk@git.com>
2017-06-06 10:04:50 +0300

    Initial commit

---
+++ My_json.py
@@ -0,0 +1,29 @@
+import json
+
+f = open('My_Json.txt', 'w')
+player1 = {
+    'Name': 'Ramon',
+    'Score': '456',
+    'Skills': ['Strent', 'GodMode']
+}
+
+player2 = {
+    'Name': 'Patrik',
+    'Score': '500',
+    'Skills': ['Invisibility', 'IronFist']
+}
+
+z = [player1,  player2]
+
+json.dump(z, f)
+
+f.close()
+
+f = open('My_Json.txt', mode='r')
+
+json_data = json.load(f)
+
+for user in json_data:
+    print('Player name is: ' + str(user['Name']))
+    print('Player score is: ' + str(user['Score']))
+    print('Player skills is: ' + str(user['Skills']))
